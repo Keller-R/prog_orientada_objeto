641e4e43124d295ff596f37b50c3c5f3ab0c96ee
Keller-R <140625890+Keller-R@users.noreply.github.com>
2023-09-15 09:42:30 -0500

    Add files via upload

---
+++ S08/Paquete1/clase2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Paquete1;
+import Paquete2.clase3;
+
+
+public class clase2 {
+    public static void main(String[] args) {
+        clase1 objc1 = new clase1();
+        clase3 objc2 = new clase3();
+        
+        objc1.a =1;
+        objc2.b = "texto";
+    }
+}
